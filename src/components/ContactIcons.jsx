5ca227937fcf868b4f93a2407a7e74f83ed88597
Nirmal-011 <nirmalsep2001@gmail.com>
2025-02-12 18:10:35 +0530

    Update the Enquiry Icon and fixed the alignment issue in Checkout page

--- src/components/ContactIcons.jsx
+++ src/components/ContactIcons.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import { FaWhatsapp, FaEnvelope,FaShoppingBasket } from 'react-icons/fa'; // Import the icons
+import { FaWhatsapp, FaEnvelope,FaShoppingBasket ,FaPhone} from 'react-icons/fa'; // Import the icons
 import './ContactIcons.css'; // Import the CSS file for styling
 
 const ContactIcons = () => {
+  const isMobile = window.innerWidth <= 768;
+
   return (
     <div>
       {/* WhatsApp Floating Button */}
@@ -23,13 +25,23 @@ const ContactIcons = () => {
       </a>
 
       {/* Enquiry Floating Button */}
+      {isMobile?
+        <a
+        href="tel:+919500759557"
+        className="enquiryfloat-button">
+            <FaPhone size={20} />
+            <span className="tooltip">Call Us</span>
+        </a>
+      :
       <a
-        href="/enquiry"
-        className="enquiryfloat-button"
-      >
-        <FaEnvelope size={20} /> {/* Reduced icon size */}
-        <span className="tooltip">Enquiry</span>
-      </a>
+      href="/enquiry"
+      className="enquiryfloat-button"
+    >
+      <FaEnvelope size={20} /> {/* Reduced icon size */}
+      <span className="tooltip">Enquiry</span>
+    </a>
+      }
+      
     </div>
   );
 };
